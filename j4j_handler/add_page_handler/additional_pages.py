bcb171dce8b4d6d469a2ed65b52ba5c790cc5357
kreuzer <t.kreuzer@fz-juelich.de>
2020-03-27 10:12:37 +0100

    bugfix authenticator (ssh account check). Add new page handler for projects and kernel tutorial

--- j4j_handler/add_page_handler/additional_pages.py
+++ j4j_handler/add_page_handler/additional_pages.py
@@ -9,8 +9,7 @@ from jupyterhub.handlers.base import BaseHandler
 
 class J4J_ToSHandler(BaseHandler):
     async def get(self):
-        user = self.current_user
-        
+        user = self.current_user        
         html = self.render_template(
                     'tos.html',
                     user=user)
@@ -18,8 +17,7 @@ class J4J_ToSHandler(BaseHandler):
         
 class J4J_DPSHandler(BaseHandler):
     async def get(self):
-        user = self.current_user
-        
+        user = self.current_user        
         html = self.render_template(
                     'dps.html',
                     user=user)
@@ -27,8 +25,7 @@ class J4J_DPSHandler(BaseHandler):
         
 class J4J_ImprintHandler(BaseHandler):
     async def get(self):
-        user = self.current_user
-        
+        user = self.current_user        
         html = self.render_template(
                     'imprint.html',
                     user=user)
@@ -36,9 +33,24 @@ class J4J_ImprintHandler(BaseHandler):
         
 class J4J_TestHandler(BaseHandler):
     async def get(self):
-        user = self.current_user
-        
+        user = self.current_user        
         html = self.render_template(
                     'test.html',
+                    user=user)
+        self.finish(html)
+        
+class J4J_ProjectsHandler(BaseHandler):
+    async def get(self):
+        user = self.current_user        
+        html = self.render_template(
+                    'projects.html',
+                    user=user)
+        self.finish(html)
+        
+class J4J_KernelHandler(BaseHandler):
+    async def get(self):
+        user = self.current_user        
+        html = self.render_template(
+                    'kernel.html',
                     user=user)
         self.finish(html)
