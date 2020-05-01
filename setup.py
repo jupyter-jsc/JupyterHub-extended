eade575782c8d93c1bf971ca564c9fed42445fe5
kreuzer <t.kreuzer@fz-juelich.de>
2020-05-01 11:34:43 +0200

    back to oauthenticator 0.8.2

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(
     author='Tim Kreuzer',
     author_email='jupyter.jsc@fz-juelich.de',
     packages=['j4j_proxy', 'j4j_authenticator', 'j4j_handler', 'j4j_spawner'],
-    install_requires=['jupyterhub>=1.1.0', 'oauthenticator>=0.8.2', 'pyjwt>=1.7.1']
+    install_requires=['jupyterhub>=1.1.0', 'oauthenticator==0.8.2', 'pyjwt>=1.7.1']
 )
