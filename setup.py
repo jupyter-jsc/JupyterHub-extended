ba3c49ddf226c97df105ef5f264aca691066404d
kreuzer <t.kreuzer@fz-juelich.de>
2020-05-01 09:18:23 +0200

    require newest version of packages

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='JupyterHub-extended',
-    version='1.0',
+    version='1.1',
     description='Extension for JupyterHub. Used for Jupyter@JSC. ',
     author='Tim Kreuzer',
     author_email='jupyter.jsc@fz-juelich.de',
     packages=['j4j_proxy', 'j4j_authenticator', 'j4j_handler', 'j4j_spawner'],
-    install_requires=['jupyterhub>=1.0.0', 'oauthenticator>=0.8.2', 'pyjwt>=1.7.1']
+    install_requires=['jupyterhub>=1.1.0', 'oauthenticator>=0.11.0', 'pyjwt>=1.7.1']
 )
